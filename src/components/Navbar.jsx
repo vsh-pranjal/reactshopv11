9325658f09b4baebf7e272c5e320fae9e82598cf
pranjal vaishnav <pranjal.pv@gmail.com>
2021-12-27 08:02:06 +0530

    title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>LAMA.</Logo>
+          <Logo>FASHAN.</Logo>
         </Center>
         <Right>
           <MenuItem>REGISTER</MenuItem>
